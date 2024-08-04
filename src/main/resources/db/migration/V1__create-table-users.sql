61f76b4f800ef4091d32938e57422cccdfd82b4c
Hugo Silva <yhugorocha@gmail.com>
2024-08-03 22:59:01 -0300

    feat: add new fields in the users table.

--- src/main/resources/db/migration/V1__create-table-users.sql
+++ src/main/resources/db/migration/V1__create-table-users.sql
@@ -4,5 +4,9 @@ CREATE TABLE users (
      password VARCHAR(255) NOT NULL,
      name VARCHAR(255) NOT NULL,
      email VARCHAR(255) NOT NULL,
-     is_active BOOLEAN NOT NULL
+     is_active BOOLEAN NOT NULL,
+     created_at TIMESTAMP NOT NULL,
+     created_by UUID NULL,
+     update_at TIMESTAMP NULL,
+     update_by UUID NULL
 );
