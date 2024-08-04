85fdd260ee39d54a43f79996c3cfa9c1f5dfd04d
Hugo Silva <yhugorocha@gmail.com>
2024-08-04 15:31:28 -0300

    feat: add unico na coluna username.

--- src/main/resources/db/migration/V1__create-table-users.sql
+++ src/main/resources/db/migration/V1__create-table-users.sql
@@ -1,6 +1,6 @@
 CREATE TABLE users (
      id UUID PRIMARY KEY,
-     username VARCHAR(255) NOT NULL,
+     username VARCHAR(255) UNIQUE NOT NULL,
      password VARCHAR(255) NOT NULL,
      name VARCHAR(255) NOT NULL,
      email VARCHAR(255) NOT NULL,
