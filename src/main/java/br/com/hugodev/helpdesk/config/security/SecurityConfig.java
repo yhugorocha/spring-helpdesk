a7ab7dc4800f91ac0d265b54e3770b45b2a7ee4a
Hugo Silva <yhugorocha@gmail.com>
2024-08-28 18:04:43 -0300

    feat: add new implementation filter JwtFilterValidator.

--- src/main/java/br/com/hugodev/helpdesk/config/security/SecurityConfig.java
+++ src/main/java/br/com/hugodev/helpdesk/config/security/SecurityConfig.java
@@ -1,5 +1,7 @@
 package br.com.hugodev.helpdesk.config.security;
 
+import br.com.hugodev.helpdesk.Util.JwtUtil;
+import br.com.hugodev.helpdesk.config.security.filter.JwtFilterValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,17 +9,17 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    private final UserDetailsService UserDetailsService;
+    private JwtUtil jwtUtil;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -25,9 +27,11 @@ public class SecurityConfig {
                 .httpBasic(Customizer.withDefaults())
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated()
-                );
+                .authorizeHttpRequests(authorize -> {
+                    authorize.requestMatchers("/auth").permitAll();
+                    authorize.anyRequest().authenticated();
 
+                }).addFilterBefore(new JwtFilterValidator(jwtUtil), BasicAuthenticationFilter.class);
 
         return http.build();
     }
