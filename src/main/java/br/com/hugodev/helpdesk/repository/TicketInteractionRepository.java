297b362dc481d9388834e04ef4f0025d6e8b99e3
Hugo Silva <yhugorocha@gmail.com>
2024-08-28 19:17:46 -0300

    feat: add new method findAllByTicketId in TicketInteractionRepository.

--- src/main/java/br/com/hugodev/helpdesk/repository/TicketInteractionRepository.java
+++ src/main/java/br/com/hugodev/helpdesk/repository/TicketInteractionRepository.java
@@ -4,8 +4,10 @@ import br.com.hugodev.helpdesk.entity.TicketInteractionEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface TicketInteractionRepository extends JpaRepository<TicketInteractionEntity, UUID> {
+    List<TicketInteractionEntity> findAllByTicketId(UUID idTicket);
 }
