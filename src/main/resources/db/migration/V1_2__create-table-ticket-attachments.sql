49388ab2e3f13147b964112f7724f0cf7abc92be
Hugo Silva <yhugorocha@gmail.com>
2024-08-28 19:13:45 -0300

    fix: fix table ticket_attachments.

--- src/main/resources/db/migration/V1_2__create-table-ticket-attachments.sql
+++ src/main/resources/db/migration/V1_2__create-table-ticket-attachments.sql
@@ -1,8 +1,9 @@
 CREATE TABLE ticket_attachments (
      id UUID PRIMARY KEY,
-     ticket_id UUID NOT NULL,
+     ticket_id UUID NULL,
      ticket_interaction_id UUID NULL,
      filename VARCHAR(255) NOT NULL,
+     url VARCHAR(255) NOT NULL,
      created_at TIMESTAMP NOT NULL,
      created_by UUID NOT NULL,
      update_at TIMESTAMP NULL,
