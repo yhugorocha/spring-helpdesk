d8713d66b5efd6f1f6bd785b09777b8a85fb7fe0
Hugo Silva <yhugorocha@gmail.com>
2024-08-28 19:17:17 -0300

    feat: add new methods User mappers.

--- src/main/java/br/com/hugodev/helpdesk/mapper/UserMapper.java
+++ src/main/java/br/com/hugodev/helpdesk/mapper/UserMapper.java
@@ -1,15 +1,19 @@
 package br.com.hugodev.helpdesk.mapper;
 
 import br.com.hugodev.helpdesk.domain.User;
+import br.com.hugodev.helpdesk.dto.CreateUserDto;
 import br.com.hugodev.helpdesk.dto.UserDto;
 import br.com.hugodev.helpdesk.entity.UserEntity;
 import org.mapstruct.Mapper;
 
+import java.util.UUID;
+
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
-    User toDomain(UserEntity user);
-
-    UserEntity toEntity(UserDto dto);
+    User toDomain(CreateUserDto userDto);
+    User toDomain(UserEntity userEntity);
+    UserEntity toEntity(User userDomain);
+    UserDto toDto(UserEntity userEntity);
 
 }
