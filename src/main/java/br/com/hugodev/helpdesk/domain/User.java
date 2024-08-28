a181d370b5ba53cfde0ad2fcbad2bc5a7b0b1e4d
Hugo Silva <yhugorocha@gmail.com>
2024-08-28 19:09:39 -0300

    refactor: refactor record User for class.

--- src/main/java/br/com/hugodev/helpdesk/domain/User.java
+++ src/main/java/br/com/hugodev/helpdesk/domain/User.java
@@ -1,6 +1,26 @@
 package br.com.hugodev.helpdesk.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
 import java.util.UUID;
 
-public record User(UUID id, String username, String password, String name, String email) {
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class User {
+
+    private UUID id;
+    private String username;
+    private String password;
+    private String name;
+    private String email;
+    private Boolean isActive;
+    private Date createdAt;
+    private UUID createdBy;
+    private Date updateAt;
+    private UUID updateBy;
+
 }
