e1c8b6ec22dd6a744ce3c45b0d8da11e697f67b3
Hugo Silva <yhugorocha@gmail.com>
2024-08-27 21:42:09 -0300

    feat: add ticket mapper.

--- src/main/java/br/com/hugodev/helpdesk/mapper/TicketMapper.java
+++ src/main/java/br/com/hugodev/helpdesk/mapper/TicketMapper.java
@@ -2,9 +2,11 @@ package br.com.hugodev.helpdesk.mapper;
 
 import br.com.hugodev.helpdesk.domain.Ticket;
 import br.com.hugodev.helpdesk.domain.User;
+import br.com.hugodev.helpdesk.dto.CreateTicketDto;
 import br.com.hugodev.helpdesk.dto.TicketDto;
+import br.com.hugodev.helpdesk.dto.TicketInteractionDto;
 import br.com.hugodev.helpdesk.entity.TicketEntity;
-import br.com.hugodev.helpdesk.entity.UserEntity;
+import br.com.hugodev.helpdesk.entity.TicketInteractionEntity;
 import org.mapstruct.Mapper;
 
 import java.util.UUID;
@@ -12,11 +14,13 @@ import java.util.UUID;
 @Mapper(componentModel = "spring")
 public interface TicketMapper {
 
-    Ticket toDomain(TicketEntity ticket);
+    Ticket toDomain(CreateTicketDto ticketDto);
+    TicketEntity toEntity(Ticket ticketDomain);
+    TicketDto toDto(TicketEntity ticketEntity);
+    TicketInteractionDto toInteractionDto(TicketInteractionEntity ticketInteraction);
 
-    TicketEntity toEntity(TicketDto ticket);
+    User map(UUID value);
+    UUID map(User value);
 
-    User mapUser(UUID value);
 
-    UserEntity mapUserEntity(UUID value);
 }
