313de110dde0fb073c67bdbe2285462d0fce6f7e
Hugo Silva <yhugorocha@gmail.com>
2024-08-28 19:16:24 -0300

    refactor: refactor UserController.

--- src/main/java/br/com/hugodev/helpdesk/controller/UserController.java
+++ src/main/java/br/com/hugodev/helpdesk/controller/UserController.java
@@ -1,8 +1,10 @@
 package br.com.hugodev.helpdesk.controller;
 
 import br.com.hugodev.helpdesk.domain.User;
+import br.com.hugodev.helpdesk.dto.CreateUserDto;
 import br.com.hugodev.helpdesk.dto.UserDto;
 import br.com.hugodev.helpdesk.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,8 +16,9 @@ public class UserController {
 
     private final UserService userService;
 
+    @Operation(description = "This method creates a new user in the system")
     @PostMapping
-    public ResponseEntity<User> createUser(@RequestBody UserDto user){
+    public ResponseEntity<UserDto> createUser(@RequestBody CreateUserDto user){
         return ResponseEntity.ok(userService.createUser(user));
     }
 }
