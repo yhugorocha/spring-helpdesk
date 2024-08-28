e532585010d1848832969af41a6fbc72778fb621
Hugo Silva <yhugorocha@gmail.com>
2024-08-27 21:42:23 -0300

    feat: add ticket Dto.

--- src/main/java/br/com/hugodev/helpdesk/dto/TicketDto.java
+++ src/main/java/br/com/hugodev/helpdesk/dto/TicketDto.java
@@ -1,6 +1,8 @@
 package br.com.hugodev.helpdesk.dto;
 
+import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
-public record TicketDto(String subject, String description, UUID createdBy) {
+public record TicketDto(UUID id, UserDto support, String subject, String description, String status, Date createdAt, UserDto createdBy, List<TicketAttachmentDto> attachments) {
 }
